4a30328b428db50a34405290aef5e5b4dfadbf8c
Sebstein08 <steinbseb@gmail.com>
2024-11-12 13:30:05 +0100

    lget kode js til bil

--- psedukode/bil.js
+++ psedukode/bil.js
@@ -3,13 +3,39 @@
 */
 
 //  person skriver inn aldern sin
+    let skjekkalder=-12 
 //  dersom aldern < 0
-        // skriv feilmeling ()
+        // skriv feilmeling (post.tall)
+        if(skjekkalder <0){
+                console.log("skriv in et tall søre en 0")
+        }
 //  dersom aldern > 70 
-        // skriv tilbameling ()
+        // skriv tilbameling (rullator)
+        if(skjekkalder >70){
+                console.log("du kan ha rullator")
+        }
 //  dersom aldern er > 18
-        // skriv tilbameling ()
-//  dersom aldern >=7
-        // skriv tilbameling ()
-//  ellers
-        // skriv tilbameling ()
+        // skriv tilbameling (bil)
+        if(skjekkalder >18){
+                console.log("du kan kjøre bil")
+        }
+
+//  dersom aldern >7
+        // skriv tilbameling (sykkel)
+        if(alder >7){
+                console.log("du kan sykle")
+        }
+//  dersom alder er <7
+        // skriv tilbameling (bruk trehuls sykkel)
+        if(skjekkalder <7)
+                console.log("bruk tre huls sykel")
+//  eller
+        // skriv tilbakemeling (tror ikke du kan gå)
+        if(skjekkalder <1,1){
+                console.log("tror ikke du kan gå")
+        }
+
+
+function oppstart(){
+        document.querySelector("#sendknapp").onclick = skjekkalder
+}
